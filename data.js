5ac1858be80b692b42639de76d971f2f82d759b4
juniorsilvacc <juniorsilvacc@hotmail.com>
2020-08-15 01:12:54 -0300

    dados dinamicos

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulario animado com JS puro e CSS Animation",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o css grid na prática",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: high order functions",
+        duration: "54 min",
+        price: "Free"
+    },
+    {
+        id: "R3S8DEzEn6s",
+        title: "Styled components",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável this",
+        duration: "48 min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git e Github na prática",
+        duration: "33 min",
+        price: "Free"
+    }
+]
